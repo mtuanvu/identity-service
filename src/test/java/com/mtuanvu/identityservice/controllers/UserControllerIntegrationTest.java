133d5c31f249328cdfdee09eb5cc6e42ad106e7f
mtuanvu <143334278+mtuanvu@users.noreply.github.com>
2024-12-09 23:55:53 +0700

    Integration test with TestContainers

---
+++ src/test/java/com/mtuanvu/identityservice/controllers/UserControllerIntegrationTest.java
@@ -0,0 +1,99 @@
+package com.mtuanvu.identityservice.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.mtuanvu.identityservice.dto.request.UserCreateRequest;
+import com.mtuanvu.identityservice.dto.response.UserResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDate;
+
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+public class UserControllerIntegrationTest {
+    @Container
+    static final MySQLContainer<?> MY_SQL_CONTAINER = new MySQLContainer<>("mysql:latest");
+
+    @DynamicPropertySource
+    static void configureDatasource(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private UserCreateRequest request;
+    private UserResponse response;
+    private LocalDate dob;
+
+    @BeforeEach
+    void initData() {
+        dob = LocalDate.of(1990, 1, 1);
+        request = UserCreateRequest.builder()
+                .username("mtuanvu1234")
+                .firstName("John")
+                .lastName("Doe")
+                .password("mtuanvu1234")
+                .dateOfBirth(dob)
+                .build();
+
+        response = UserResponse.builder()
+                .id(1L)
+                .username("mtuanvu1234")
+                .firstName("John")
+                .lastName("Doe")
+                .dateOfBirth(dob)
+                .build();
+    }
+
+    @Test
+        //
+    void createUser_validRequest_success() throws Exception {
+        //GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String content = objectMapper.writeValueAsString(request);
+
+
+        //WHEN
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/users/create")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(content))
+
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code")
+                        .value("201"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.username")
+                        .value("mtuanvu1234"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.firstName")
+                        .value("John"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.lastName")
+                        .value("Doe"));
+
+        log.info("Result: {}", resultActions.andReturn().getResponse().getContentAsString());
+        //THEN
+    }
+}
